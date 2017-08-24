efab39e005549a62e34ec2eef7903e8e59f4ec4b
Wenter <uuooooq@gmail.com>
2017-08-25 07:37:11 +0800

    建立初始网站解析文件

---
+++ index.js
@@ -0,0 +1,12 @@
+/**
+ * Created by zhudongwei on 25/08/2017.
+ */
+const Koa = require('koa');
+const app = new Koa();
+
+// response
+app.use(ctx => {
+    ctx.body = '网站正在建设中...';
+});
+
+app.listen(3000);
